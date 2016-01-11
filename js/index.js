63fc900d6c40c01f4dfad9d85a55f9d83b69966f
Jill Russek <jill@th.ru.st>
2016-01-11 16:00:38 -0500

    cookie set on browswer refresh

--- js/index.js
+++ js/index.js
@@ -14,14 +14,29 @@ app.controller('myCtrl', ['$scope', '$http', '$cookies',
 function($scope,$http, $cookies) {
 	$scope.sendPost = function() {
 
-	// Set cookie for search term
-	$cookies.put('search_term' , $scope.search_term);
+		// Set cookie for search term
+		$cookies.put('search_term' , $scope.search_term);
 
-	// Fetch results for search term
-	$http.post("/", {text:$scope.search_term}).then(function(response){
-		$scope.stati = response.data.stati;
-		$scope.error_message = response.data.error_message;
+		// Fetch results for search term
+		$http.post("/", {text:$scope.search_term}).then(function(response){
+			$scope.stati = response.data.stati;
+			$scope.error_message = response.data.error_message;
 		});//end response and post
-
 	};//end sendPost
+
+	// Check for cookie and reload results
+	var init = function () {
+		search_term =  $cookies.get('search_term');
+		if(search_term){
+			// Fetch results for search term
+			$http.post("/", {text:search_term}).then(function(response){
+				$scope.stati = response.data.stati;
+				$scope.error_message = response.data.error_message;
+				$scope.search_term = search_term;
+			});//end response and post
+		}
+	};
+		// and fire it after definition
+	init();
+	
 }]);//end controller
